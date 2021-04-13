e2472f477607057648b33e76f6f64c107c6f1f9d
helx <lixiang1137@163.com>
2021-04-13 22:26:10 +0800

    增加一个喵喵喵

---
+++ src/main/java/cn/helx/learnactivemqdemo/mycat/tomcat.java
@@ -0,0 +1,12 @@
+package cn.helx.learnactivemqdemo.mycat;
+
+/**
+ * @author helx
+ * @description cn.helx.learnactivemqdemo.mycat
+ * @create 2021-04-13 22:25
+ */
+public class tomcat {
+    public static void main(String[] args) {
+        System.out.println("喵喵喵");
+    }
+}
